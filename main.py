84485f4f599c5d7ab8cf39aa9efadbd6912f4809
Elias <elias.mueller@mailbox.org>
2023-11-29 19:31:31 +0100

    Make it work under Windows 10

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
+# Copyright 2023
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+
 import sys
 
 from PySide6.QtCore import QUrl
 from PySide6.QtGui import QGuiApplication
 from PySide6.QtQml import QQmlApplicationEngine
+
+from win import WindowsEventFilter as EventFilter
+from win import WindowsWindowEffect as WindowEffect
 
 
 class MyApp(QGuiApplication):
@@ -11,8 +29,16 @@ class MyApp(QGuiApplication):
         super().__init__()
         self._engine = QQmlApplicationEngine()
 
+        self.event_filter = EventFilter(5)
+        self.installNativeEventFilter(self.event_filter)
+        self.win_utilities = WindowEffect()
+
     def start_engine(self):
         self._engine.load(QUrl.fromLocalFile('app.qml'))
+
+        for win in self.allWindows():
+            self.win_utilities.addShadowEffect(win.winId())
+            self.win_utilities.addWindowAnimation(win.winId())
 
     def verify(self):
         if not self._engine.rootObjects():
